64cacadb7eef80c17f13893a542ea04cb15adde2
David White <dwhite0627@gmail.com>
2020-05-15 20:32:26 -0400

    Fix test database paths

--- knexfile.js
+++ knexfile.js
@@ -1,41 +1,38 @@
-
 module.exports = {
-
   development: {
-    client: 'sqlite3',
-    connection: { filename: './database/auth.db3' },
+    client: "sqlite3",
+    connection: { filename: "./database/auth.db3" },
     useNullAsDefault: true,
     migrations: {
-      directory: './database/migrations',
-      tableName: 'dbmigrations',
+      directory: "./database/migrations",
+      tableName: "dbmigrations",
     },
-    seeds: { directory: './database/seeds' },
+    seeds: { directory: "./database/seeds" },
     // needed when using foreign keys
     pool: {
       afterCreate: (conn, done) => {
         // runs after a connection is made to the sqlite engine
-        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+        conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
       },
     },
   },
   testing: {
-
-    client: 'sqlite3',
-    useNullAsDefault: true, 
+    client: "sqlite3",
+    useNullAsDefault: true,
     connection: {
-      filename: './data/users-test.db3',
+      filename: "./database/users-test.db3",
     },
     migrations: {
-      directory: './data/migrations'
+      directory: "./database/migrations",
     },
     seeds: {
-      directory: './data/seeds'
+      directory: "./database/seeds",
     },
     // needed when using foreign keys
     pool: {
       afterCreate: (conn, done) => {
         // runs after a connection is made to the sqlite engine
-        conn.run('PRAGMA foreign_keys = ON', done); // turn on FK enforcement
+        conn.run("PRAGMA foreign_keys = ON", done); // turn on FK enforcement
       },
     },
   },
