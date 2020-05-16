9ee3b8d91eab2172860f126650b1b0404db55702
David White <dwhite0627@gmail.com>
2020-05-15 20:13:11 -0400

    Add validateUsername local middleware to validate usernames when logging in

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -27,6 +27,29 @@ router.post("/register", async (req, res) => {
   }
 });
 
+/**
+ * @function validateUsername: Validate the the id exists before submitting req
+ * @param {*} req: The request object sent to the API
+ * @param {*} res: The response object sent from the API
+ * @param {*} next: The express middleware function to move to the next middleware
+ * @returns: none
+ */
+async function validateUsername(req, res, next) {
+  try {
+    const { username } = req.body;
+    const user = await findBy({ username });
+    if (!user) {
+      return res.status(404).json({
+        message: "Not Found",
+        validation: ["Username doesn't exist"],
+        data: {},
+      });
+    }
+    next();
+  } catch (err) {
+    errDetail(res, err);
+  }
+}
 function errDetail(res, err) {
   console.log(err);
   return res.status(500).json({
