94601dd4d315e5c046f7596021d4edc9987ce258
David White <dwhite0627@gmail.com>
2020-05-15 21:44:21 -0400

    Modify the default App.js and index.js for the React app

---
+++ client/src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+// Component imports
+import Jokes from './components/Jokes';
+
+function App() {
+  return (
+    <div className="App">
+      <Jokes />
+    </div>
+  );
+}
+
+export default App;
