7c28b75d216d3e26ea16ffc46367a2c77b4f063c
David White <dwhite0627@gmail.com>
2020-05-15 20:12:33 -0400

    Add the register route

--- auth/auth-router.js
+++ auth/auth-router.js
@@ -1,11 +1,39 @@
-const router = require('express').Router();
+const express = require("express");
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const router = express.Router();
 
-router.post('/register', (req, res) => {
-  // implement registration
+// Subroutes
+const restricted = require("./authenticate-middleware");
+const jokesRoute = require("../jokes/jokes-router");
+router.use("/jokes", restricted(), jokesRoute);
+
+// Db helper fns
+const { findBy, insert } = require("./../users/users-model");
+
+router.post("/register", async (req, res) => {
+  try {
+    const user = req.body;
+    const hash = bcrypt.hashSync(req.body.password, Number(process.env.HASHES));
+    user.password = hash;
+    const newUser = await insert(user);
+    res.status(201).json({
+      message: `Registered ${newUser.username} successfully`,
+      validation: [],
+      data: newUser,
+    });
+  } catch (err) {
+    errDetail(res, err);
+  }
 });
 
-router.post('/login', (req, res) => {
-  // implement login
-});
+function errDetail(res, err) {
+  console.log(err);
+  return res.status(500).json({
+    message: "There was a problem completing the required operation",
+    validation: [],
+    data: {},
+  });
+}
 
 module.exports = router;
